a7d8bc5475e2defa8f502a249e97eb13dc26362a
Mark Abramov <markabramov01@gmail.com>
2019-03-18 23:06:55 -0400

    add word counter -- turns red if over character limit

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -1,12 +1,16 @@
 package com.codepath.apps.restclienttemplate;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
@@ -24,6 +28,7 @@ public class ComposeActivity extends AppCompatActivity {
 
     private EditText etCompose;
     private Button btnTweet;
+    private TextView tvCount;
     private TwitterClient client;
 
     @Override
@@ -34,6 +39,27 @@ public class ComposeActivity extends AppCompatActivity {
         client = TwitterApp.getRestClient(this);
         etCompose = findViewById(R.id.etCompose);
         btnTweet = findViewById(R.id.btnTweet);
+        tvCount = findViewById(R.id.tvCount);
+
+        etCompose.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                int length = etCompose.getText().toString().length();
+                String message = length + "/" + MAX_TWEET_LENGTH;
+                tvCount.setText(message);
+                tvCount.setTextColor(length < MAX_TWEET_LENGTH ? Color.BLACK: Color.RED);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
         btnTweet.setOnClickListener(new View.OnClickListener() {
             @Override
